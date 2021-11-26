5b63c6db346b08fbdaa2abcc50777c0869bb0fe5
dianajlailaty <66261096+dianajlailaty@users.noreply.github.com>
2021-11-26 16:32:32 +0100

    Update dash_utils.py

--- maas_ml/dash_utils.py
+++ maas_ml/dash_utils.py
@@ -3,10 +3,12 @@ import json
 import pandas as pd
 import re
 import statistics
-
+import numpy as np
+import flask
 
 from datetime import datetime as dt
 from os.path import join, exists, isfile
+from tempfile import TemporaryFile
 from scipy import stats
 from os import environ, path
 
@@ -20,12 +22,14 @@ from dash.dependencies import Output
 import plotly.graph_objs as go
 import plotly.express as px
 
+
 # Helper function
 def alphanum_sort(l):
     convert = lambda text: int(text) if text.isdigit() else text
     alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
     return sorted(l, key=alphanum_key)
 
+INSTANCE_PATH = os.getenv('INSTANCE_PATH') if os.getenv('INSTANCE_PATH') is not None else None
 # Dash app functions
 
 # Main Dash/plotly layout page
@@ -55,10 +59,12 @@ def serve_layout():
         # 'border-radius': '20px',
         'padding': '20px'
     }
-    
+
     # Tabs of the Dash app page
     return html.Div([
-        dcc.Tabs(id='tabs-example', value='tab-1', children=[
+        dcc.Tabs(id='tabs-example', value='tab-0', children=[
+            dcc.Tab(label='Audit and Traceability', value='tab-0', style=tab_style,
+                    selected_style=tab_selected_style),
             dcc.Tab(label='Dataset Analytics', value='tab-1', style=tab_style,
                     selected_style=tab_selected_style),
             dcc.Tab(label='Data Drift Analytics', value='tab-2', style=tab_style,
@@ -89,12 +95,35 @@ def init_dashboard(server):
 
     app.layout = serve_layout
 
-    
     # All callback functions that are executed once any event takes place in the dash app components
     @app.callback(Output('tabs-example-content', 'children'), Input('tabs-example', 'value'))
     def render_content(tab):
+        url = flask.request.host_url
         directory_contents = os.listdir(os.environ['MODELS_PATH'])
-        if tab == 'tab-1':
+        if tab == 'tab-0':
+            return html.Div([
+                dcc.Interval(
+                    id='interval-component',
+                    interval=10 * 1000,  # in milliseconds
+                    n_intervals=0
+                ),
+                #html.Div(id='textarea-example-output', style={'whiteSpace': 'pre-line'}),
+                html.Br(),
+                html.Br(),
+                html.A(html.Button('Go to Swagger UI', className='three columns'),
+                       href='ui/#/default'),
+                html.Br(),
+                html.Br(),
+                html.Label(["Configuration Details"], style={'font-weight': 'bold', "text-align": "center", "font-size": '3rem'}),
+                html.Br(),
+                html.Div(id='config-table'),
+                html.Br(),
+                html.Br(),
+                html.Label(["Traceability Details"], style={'font-weight': 'bold', "text-align": "center", "font-size": '3rem'}),
+                html.Br(),
+                html.Div(id='trace-table')
+            ])
+        elif tab == 'tab-1':
             return html.Div([
                 dcc.Interval(
                     id='interval-component',
@@ -276,6 +305,80 @@ def init_dashboard(server):
         else:
             return [], 'No predictions yet'
 
+
+
+    @app.callback(Output('config-table', 'children'),
+                  Input('interval-component', 'n_intervals'))
+    def config_table(n):
+        path = INSTANCE_PATH + "/config.json"
+        with open(path) as f:
+            config = json.load(f)
+        dataframe_config = pd.DataFrame.from_records([config])
+        columns = dataframe_config.columns.tolist()
+        columnss = list()
+        for i in range(0, len(columns)):
+            columnss.append({"name": str(columns[i]), "id": str(columns[i]), })
+        data = dataframe_config.to_dict('rows')
+        d = [{key: str(value) for key, value in data[0].items()}]
+        return dash_table.DataTable(data=d, columns=columnss,
+                                    style_cell={'padding': '20px', 'border': '1px solid #E6E6FA', 'textAlign': 'center'},
+                                    style_header={
+                                        'backgroundColor': '#E6E6FA',
+                                        'fontWeight': 'bold'
+                                    })
+
+    @app.callback(Output('trace-table', 'children'),
+                  Input('interval-component', 'n_intervals'))
+    def trace_table(n):
+        filename = INSTANCE_PATH + "/trace.txt"
+        header = ["Date Time", "Token", "Traceability information"]
+        #return filename
+        if exists(filename) and isfile(filename):
+            dataframe_trace = pd.read_csv(filename, sep="|", names=header, engine='python')
+            dataframe_trace.fillna('', inplace=True)
+        else:
+            dataframe_trace = pd.DataFrame(np.array([['', '', '[WARN] Trace file is empty']]), columns=header)
+
+        columns = dataframe_trace.columns.tolist()
+        columnss = list()
+        for i in range(0, len(columns)):
+            columnss.append({"name": str(columns[i]), "id": str(columns[i]), })
+        data = dataframe_trace.to_dict('rows')
+        l = list()
+        for d in data:
+            d = {key: str(value) for key, value in d.items()}
+            l.append(d)
+        return dash_table.DataTable(data=l, columns=columnss,
+                                    style_cell={'padding': '20px', 'border': '1px solid #E6E6FA', 'textAlign': 'center'},
+                                    style_header={
+                                        'backgroundColor': '#E6E6FA',
+                                        'fontWeight': 'bold'
+                                    })
+
+
+
+
+
+
+
+
+        # with open(path) as f:
+        #     config = json.load(f)
+        # dataframe_config = pd.DataFrame.from_records([config])
+        # columns = dataframe_config.columns.tolist()
+        # columnss = list()
+        # for i in range(0, len(columns)):
+        #    columnss.append({"name": str(columns[i]), "id": str(columns[i]), })
+        # data = dataframe_config.to_dict('rows')
+        # d = [{key: str(value) for key, value in data[0].items()}]
+        # # return str(d)
+        # return dash_table.DataTable(data=d, columns=columnss,
+        #                             style_cell={'padding': '20px', 'border': '1px solid #E6E6FA'},
+        #                             style_header={
+        #                                 'backgroundColor': '#E6E6FA',
+        #                                 'fontWeight': 'bold'
+        #                             })
+
     @app.callback(Output('predictions-table', 'children'),
                   Input('prediction-model-dropdown', 'value'), Input('version-dropdown', 'value'),
                   Input('version-dropdown', 'placeholder'),
@@ -286,13 +389,12 @@ def init_dashboard(server):
         cols = list()
         path = os.environ['MODELS_PATH'] + "/" + name + "/" + version + "/" + "predictions_data_" + version + ".csv"
         predictions_dataframe = pd.read_csv(path, header=None)
-        predictions_dataframe = predictions_dataframe.iloc[:, :-1]
         data = predictions_dataframe.to_dict('rows')
         for i in range(0, (len(predictions_dataframe.columns))):
             cols.append(str(i))
         columns = [{"name": i, "id": i, } for i in cols]
         return dash_table.DataTable(data=data, columns=columns, style_as_list_view=True,
-                                    style_cell={'padding': '20px', 'border': '1px solid #E6E6FA'},
+                                    style_cell={'padding': '20px', 'border': '1px solid #E6E6FA', 'textAlign': 'center'},
                                     style_header={
                                         'backgroundColor': '#E6E6FA',
                                         'fontWeight': 'bold'
