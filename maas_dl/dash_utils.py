ac315e97bc790e6dd18f02b8a2fbb55557a31508
ibizid <imen.bizid.chaibi@gmail.com>
2022-06-20 10:43:54 +0200

    update maas_ml and maas_dl dashboards

--- maas_dl/dash_utils.py
+++ maas_dl/dash_utils.py
@@ -34,7 +34,7 @@ def alphanum_sort(l):
 INSTANCE_PATH = os.getenv('INSTANCE_PATH') if os.getenv('INSTANCE_PATH') is not None else None
 # Dash app functions
 
-image_filename = '/model_as_service/activeeon-logo-orange-blue-trasparent.png'
+image_filename = '/model_as_service/static/img/activeeon-logo-orange-blue-trasparent.png'
 
 def b64_image(image_filename):
     with open(image_filename, 'rb') as f:
@@ -44,7 +44,7 @@ def b64_image(image_filename):
 # Main Dash/plotly layout page
 def serve_layout():
     tab_style = {
-        "background": "#E6E6FA",
+        "background": "#d6d6d6",
         'text-transform': 'uppercase',
         'color': 'black',
         'border': 'black',
@@ -53,11 +53,11 @@ def serve_layout():
         'align-items': 'center',
         'justify-content': 'center',
         # 'border-radius': '20px',
-        #'padding': '20px'
+        'padding': '20px'
     }
 
     tab_selected_style = {
-        "background": "#1C84C6",
+        "background": "rgb(28, 132, 198)",
         'text-transform': 'uppercase',
         'color': 'white',
         'border': 'black',
@@ -67,6 +67,7 @@ def serve_layout():
         'justify-content': 'center',
         # 'border-radius': '20px',
         #'padding': '20px'
+        'padding': '20px'
     }
     header_style = {
         'background-color': "#F3F3F4",
@@ -102,7 +103,7 @@ def serve_layout():
         html.Div(children=[
             html.Div(children=[html.Img(src=b64_image(image_filename),style={'height': "40px"})], style=image_container_style),
             html.Div(children=[
-                html.H1(children='Machine Learning Model as a Service', style={'font-weight': "400",'font-size': '30px'})], style=header_text_style)], style=header_style),
+                html.H1(children='Deep Learning as a Service', style={'font-weight': "400",'font-size': '30px'})], style=header_text_style)], style=header_style),
         html.Div(style=line_separator_style),
         html.Div(children=[
             dcc.Tabs(id='tabs-example', value='tab-0', children=[
@@ -141,19 +142,19 @@ def init_dashboard(server):
                 html.Br(),
                 html.Br(),
                 html.Div(children=[
-                    html.A(html.Button("Go to Swagger UI", className='three columns'),
-                       href='/api/ui/#/default', target="_blank")], style={'padding-left': "10px"}
+                    html.A(html.Button("Go to Swagger UI", className='two columns', style={'background-color': '#1AB394', 'color': '#FFF', 'float': 'right', 'margin-right':'20px'}),
+                       href='/api/ui/#/default', target="_blank")]#, style={'float': 'right', 'margin-right':'10px'}
                 ),
                 html.Br(),
                 html.Br(),
                 html.Label(["Configuration Details"], style={'font-weight': 'bold', "text-align": "center", "font-size": '3rem'}),
                 html.Br(),
-                html.Div(id='config-table',style={'padding-left': "10px",'padding-right': "10px"}),
+                html.Div(id='config-table', style={'padding-left': "10px",'padding-right': "10px"}),
                 html.Br(),
                 html.Br(),
                 html.Label(["Traceability Details"], style={'font-weight': 'bold', "text-align": "center", "font-size": '3rem'}),
                 html.Br(),
-                html.Div(id='trace-table',style={'padding-left': "10px",'padding-right': "10px"})
+                html.Div(id='trace-table', style={'padding-left': "10px",'padding-right': "10px"})
                 #]))
             ])
         elif tab == 'tab-1':
@@ -191,7 +192,7 @@ def init_dashboard(server):
         return dash_table.DataTable(data=d, columns=columnss,
                                     style_cell={'padding': '20px', 'border': '1px solid #E6E6FA', 'textAlign': 'center'},
                                     style_header={
-                                        'backgroundColor': '#D6D6D6',
+                                        'backgroundColor': '#f5f5f5',
                                         'fontWeight': 'bold'
                                     })
 
@@ -218,6 +219,6 @@ def init_dashboard(server):
         return dash_table.DataTable(data=l, columns=columnss,
                                     style_cell={'padding': '20px', 'border': '1px solid #E6E6FA', 'textAlign': 'center'},
                                     style_header={
-                                        'backgroundColor': '#D6D6D6',
+                                        'backgroundColor': '#f5f5f5',
                                         'fontWeight': 'bold'
                                     })
