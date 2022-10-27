5c668cd4650429929528475c948c062778e90099
mklkun <mohamed.khalil.labidi@gmail.com>
2022-10-27 15:34:00 +0200

    Delete run.sh

--- sal/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -Djavax.net.ssl.trustStore=/config/common/melodic-truststore.p12 -Djavax.net.ssl.trustStorePassword=melodic -Djavax.net.ssl.trustStoreType=pkcs12 -Duser.timezone=Europe/Warsaw -Djava.security.egd=file:/dev/./urandom -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar  /app/spring-boot-application.war --spring.config.location=file:/app/morphemic.application.properties
