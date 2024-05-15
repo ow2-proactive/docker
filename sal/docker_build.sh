bf41b8c9d2a6ac9de650a4e079974a20a5d01abb
mbenguig <michael.benguigui@activeeon.com>
2024-05-15 14:09:19 +0200

    Update sal docker image tag

--- sal/docker_build.sh
+++ sal/docker_build.sh
@@ -1,6 +1,5 @@
 build_env="dev"
 build_date=$(date '+%Y-%m-%d')
-build_version="123"
-build_tag=${build_env}"-"${build_date}"."${build_version}
+build_tag=${build_env}"-"${build_date}
 echo $build_tag
 docker build -t activeeon/sal:$build_tag -f ./Dockerfile --no-cache .
