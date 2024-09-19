37f779b38eeb5998c98516a2ed33f738b67ff6db
Andrews Cordolino Sobral <andrewssobral@gmail.com>
2024-09-19 15:10:27 +0200

    Added grpc and http triton clients for iris classification model, updated Makefile and aux scripts

---
+++ tritonserver/22.10-py3-ddd/client/test_add_sub_client.sh
@@ -0,0 +1,2 @@
+./triton_add_sub_client.py "localhost:18000"
+./triton_add_sub_client.py "localhost:18080"
---
+++ tritonserver/22.10-py3-ddd/client/test_sum_client.sh
@@ -0,0 +1,2 @@
+./triton_sum_client.py "localhost:18000"
+./triton_sum_client.py "localhost:18080"
